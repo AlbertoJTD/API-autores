2523a96b285b215427382c569047268bf5412b14
AlbertoJTD <albertojtd@outlook.com>
2023-05-07 14:39:22 -0600

    Add class and Autor controller

---
+++ WebApiAutores/Controllers/AutoresController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebApiAutores.Entidades; // Tener acceso a la clase autor
+
+namespace WebApiAutores.Controllers
+{
+    [ApiController]
+    [Route("api/autores")] // Este es el ENDPOINT
+    public class AutoresController: ControllerBase
+    {
+        [HttpGet] //Accion
+        public ActionResult<List<Autor>> Get() // Retorna un listado de 2 autores cuando se haga una peticion GET
+        {
+            return new List<Autor>() { 
+                new Autor() { Id = 1, Nombre = "Felipe" },
+                new Autor() { Id = 2, Nombre = "Juan" },
+            };
+        }
+    }
+}
---
+++ WebApiAutores/Entidades/Autor.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApiAutores.Entidades
+{
+    public class Autor
+    {
+        public int Id { get; set; } // Propiedad Id
+        public string Nombre { get; set; } // Propiedad Nombre
+
+    }
+}
