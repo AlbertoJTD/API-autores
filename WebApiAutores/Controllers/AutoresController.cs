5b233ad7f7378cf1c4c477848940b4987533b549
AlbertoJTD <albertojtd@outlook.com>
2023-05-07 16:14:50 -0600

    Add GET, POST, PUT and DELETE methods

--- WebApiAutores/Controllers/AutoresController.cs
+++ WebApiAutores/Controllers/AutoresController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using WebApiAutores.Entidades; // Tener acceso a la clase autor
 
 namespace WebApiAutores.Controllers
@@ -8,13 +10,59 @@ namespace WebApiAutores.Controllers
     [Route("api/autores")] // Este es el ENDPOINT
     public class AutoresController: ControllerBase
     {
-        [HttpGet] //Accion
-        public ActionResult<List<Autor>> Get() // Retorna un listado de 2 autores cuando se haga una peticion GET
+        private readonly ApplicationDbContext context;
+
+        public AutoresController(ApplicationDbContext context)
         {
-            return new List<Autor>() { 
-                new Autor() { Id = 1, Nombre = "Felipe" },
-                new Autor() { Id = 2, Nombre = "Juan" },
-            };
+            this.context = context;
+        }
+
+        [HttpGet] //Accion
+        public async Task<ActionResult<List<Autor>>> Get() // Retorna un listado de 2 autores cuando se haga una peticion GET
+        {
+            return await context.Autores.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Autor autor)
+        {
+            context.Add(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPut("{id:int}")] // Se combina la ruta del ENDPOINT + lo que se coloca aqui => api/autores/1
+        public async Task<ActionResult> Put(Autor autor, int id)
+        {
+            if (autor.Id != id) { 
+                return BadRequest("El ID del autor no coincide con el ID de la URL");
+            }
+
+            var existe = await context.Autores.AnyAsync(x => x.Id == id);
+
+            if (!existe)
+            {
+                return NotFound();
+            }
+
+            context.Update(autor); // Actualiza el objeto
+            await context.SaveChangesAsync(); // Se actualliza el registro en la DB
+            return Ok();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(Autor autor, int id)
+        {
+            var existe = await context.Autores.AnyAsync(x => x.Id == id);
+
+            if (!existe)
+            {
+                return NotFound();
+            }
+
+            context.Remove(new Autor() { Id = id });
+            await context.SaveChangesAsync();
+            return Ok();
         }
     }
 }
