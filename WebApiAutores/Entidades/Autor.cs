3d60eb7245402545b7e963d576d1592c0d1127cc
AlbertoJTD <albertojtd@outlook.com>
2023-05-08 09:39:05 -0600

    Add default validations

--- WebApiAutores/Entidades/Autor.cs
+++ WebApiAutores/Entidades/Autor.cs
@@ -1,11 +1,29 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebApiAutores.Entidades
 {
     public class Autor
     {
         public int Id { get; set; } // Propiedad Id
+
+        [Required(ErrorMessage = "El campo nombre es requerido")] // El atributo nombre es requerido
+        [StringLength(maximumLength:5, ErrorMessage = "El campo {0} no debe de tener mas de {1} caracteres")]
         public string Nombre { get; set; } // Propiedad Nombre
+
+        [Range(18, 100)]
+        [NotMapped] // Propiedades que no van a corresponder con una columna en la BD
+        public int Edad { get; set; }
+
+        [CreditCard]
+        [NotMapped]
+        public string TarjetaDeCredito { get; set; }
+
+        [Url]
+        [NotMapped]
+        public string URL { get; set; }
+
         public List<Libro> Libros { get; set; }
     }
 }
