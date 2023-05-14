6a839b3b7a00e4e0b7b9a2a84340c961e3e421da
AlbertoJTD <albertojtd@outlook.com>
2023-05-14 13:23:10 -0600

    Add implementation for HATEOAS

---
+++ WebApiAutores/Controllers/RootController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Collections.Generic;
+using WebApiAutores.DTOs;
+
+namespace WebApiAutores.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class RootController: ControllerBase
+    {
+        [HttpGet(Name = "ObtenerRoot")]
+        public ActionResult<IEnumerable<DatoHATEOAS>> Get()
+        {
+            var datosHATEOAS = new List<DatoHATEOAS>();
+            datosHATEOAS.Add(new DatoHATEOAS(enlace: Url.Link("ObtenerRoot", new {}), descripcion: "self", metodo: "GET"));
+
+            return datosHATEOAS;
+        }
+    }
+}
