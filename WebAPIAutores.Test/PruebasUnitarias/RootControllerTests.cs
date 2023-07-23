dac49e988ea65787f07dc238a3931279d7a28f72
AlbertoJTD <albertojtd@outlook.com>
2023-07-23 13:10:26 -0600

    Add mocks using Moq

--- WebAPIAutores.Test/PruebasUnitarias/RootControllerTests.cs
+++ WebAPIAutores.Test/PruebasUnitarias/RootControllerTests.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using WebApiAutores.Controllers.V1;
@@ -36,6 +40,36 @@ namespace WebAPIAutores.Test.PruebasUnitarias
 			authorizationService.Resultado = AuthorizationResult.Failed();
 			var rootController = new RootController(authorizationService);
 			rootController.Url = new URLHelperMock();
+
+			// Ejecucion
+			var result = await rootController.Get();
+
+			// Verificacion
+			Assert.AreEqual(2, result.Value.Count());
+		}
+
+		[TestMethod]
+		public async Task IfUserIsNotAdmin_Get2Links_UsingMoq()
+		{
+			// Preparacion
+			var mockAuthorizationService = new Mock<IAuthorizationService>();
+			mockAuthorizationService.Setup(x => x.AuthorizeAsync(It.IsAny<ClaimsPrincipal>(),
+											It.IsAny<object>(),
+											It.IsAny<IEnumerable<IAuthorizationRequirement>>()))
+											.Returns(Task.FromResult(AuthorizationResult.Failed()));
+
+			mockAuthorizationService.Setup(x => x.AuthorizeAsync(It.IsAny<ClaimsPrincipal>(),
+											It.IsAny<object>(),
+											It.IsAny<string>()))
+											.Returns(Task.FromResult(AuthorizationResult.Failed()));
+
+			var mockURLHelper = new Mock<IUrlHelper>();
+			mockURLHelper.Setup(x => x.Link(It.IsAny<string>(),
+											It.IsAny<object>())).
+											Returns(string.Empty);
+
+			var rootController = new RootController(mockAuthorizationService.Object);
+			rootController.Url = mockURLHelper.Object;
 
 			// Ejecucion
 			var result = await rootController.Get();
