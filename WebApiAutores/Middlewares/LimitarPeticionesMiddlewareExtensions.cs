7b235a015bd348137b25775de50ba93e12b815fd
AlbertoJTD <albertojtd@outlook.com>
2023-07-31 20:35:23 -0600

    Limit requests pe day

--- WebApiAutores/Middlewares/LimitarPeticionesMiddlewareExtensions.cs
+++ WebApiAutores/Middlewares/LimitarPeticionesMiddlewareExtensions.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using System;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using WebApiAutores;
 using WebApiAutores.DTOs;
+using WebApiAutores.Entidades;
 
 namespace WebApiAutores.Middlewares
 {
@@ -41,6 +44,52 @@ public class LimitarPeticionesMiddleware
 			await httpContext.Response.WriteAsync("Debe proveer la llave en la cabecera X-Api-Key");
 			return;
 		}
+
+		if (llaveStringValues.Count > 1)
+		{
+			httpContext.Response.StatusCode = 400;
+			await httpContext.Response.WriteAsync("Solo una llave debe estar presente");
+			return;
+		}
+
+		var llave = llaveStringValues[0];
+		var llaveDB = await context.LlavesAPI.FirstOrDefaultAsync(x => x.Llave == llave);
+
+		if (llaveDB == null)
+		{
+			httpContext.Response.StatusCode = 400;
+			await httpContext.Response.WriteAsync("La llave no existe");
+			return;
+		}
+
+		if (!llaveDB.Activa)
+		{
+			httpContext.Response.StatusCode = 400;
+			await httpContext.Response.WriteAsync("La llave se encuentra inactiva");
+			return;
+		}
+
+		if (llaveDB.TipoLlave == TipoLlave.Gratuita)
+		{
+			var hoy = DateTime.Today;
+			var manana = hoy.AddDays(1);
+			var cantidadPeticionesRealizadasHoy = await context.Peticiones.CountAsync(x =>  x.LlaveId == llaveDB.Id && x.FechaPeticion >= hoy && x.FechaPeticion < manana);
+
+			if (cantidadPeticionesRealizadasHoy >= limitarPeticionesConfiguracion.PeticionesPorDiaGratuito)
+			{
+				httpContext.Response.StatusCode = 429; // Too many requests
+				await httpContext.Response.WriteAsync("Ha excedido el numero de peticiones por dia. Actualice su cuenta a Profesional para realizar mas peticiones");
+				return;
+			}
+		}
+
+		var peticion = new Peticion()
+		{
+			LlaveId = llaveDB.Id,
+			FechaPeticion = DateTime.UtcNow
+		};
+		context.Add(peticion);
+		await context.SaveChangesAsync();
 
 		await siguiente(httpContext);
 	}
