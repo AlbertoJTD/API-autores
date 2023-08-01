9291d8530d5d719bb7e297dd802f6e2853613ca7
AlbertoJTD <albertojtd@outlook.com>
2023-08-01 10:26:32 -0600

    Add Post method to RestriccionesDominioController

---
+++ WebApiAutores/Controllers/RestriccionesDominioController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using WebApiAutores.Controllers.V1;
+using WebApiAutores.DTOs;
+using WebApiAutores.Entidades;
+
+namespace WebApiAutores.Controllers
+{
+	[ApiController]
+	[Route("api/v1/restriccionesdominio")]
+	[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+	public class RestriccionesDominioController: CustomBaseController
+	{
+		private readonly ApplicationDbContext context;
+
+		public RestriccionesDominioController(ApplicationDbContext context)
+        {
+			this.context = context;
+		}
+
+		[HttpPost]
+		public async Task<ActionResult> Post(CrearRestriccionesDominioDTO crearRestriccionesDominioDTO)
+		{
+			var llaveDB = await context.LlavesAPI.FirstOrDefaultAsync(x => x.Id == crearRestriccionesDominioDTO.LlaveId);
+			if (llaveDB == null)
+			{
+				return NotFound();
+			}
+
+			var usuarioId = ObtenerUsuarioId();
+			if (llaveDB.UsuarioId != usuarioId)
+			{
+				return Forbid();
+			}
+
+			var restriccionDominio = new RestriccionDominio()
+			{
+				LlaveId = crearRestriccionesDominioDTO.LlaveId,
+				Dominio = crearRestriccionesDominioDTO.Dominio
+			};
+
+			context.Add(restriccionDominio);
+			await context.SaveChangesAsync();
+			return NoContent();
+		}
+	}
+}
