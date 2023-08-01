764103c620d8450bae9b3346f6f5070e6a53e5d5
AlbertoJTD <albertojtd@outlook.com>
2023-08-01 10:33:50 -0600

    Add Delete method to ActualizarRestriccionDominioDTO

--- WebApiAutores/Controllers/RestriccionesDominioController.cs
+++ WebApiAutores/Controllers/RestriccionesDominioController.cs
@@ -67,5 +67,26 @@ namespace WebApiAutores.Controllers
 			await context.SaveChangesAsync();
 			return NoContent();
 		}
+
+		[HttpDelete("{id:int}")]
+		public async Task<ActionResult> Delete(int id)
+		{
+			var restriccionDB = await context.RestriccionesDominio.Include(x => x.Llave).FirstOrDefaultAsync(x => x.Id == id);
+			if (restriccionDB == null)
+			{
+				return NotFound();
+			}
+
+			var usuarioId = ObtenerUsuarioId();
+			if (restriccionDB.Llave.UsuarioId != usuarioId)
+			{
+				return Forbid();
+			}
+
+			context.Remove(restriccionDB);
+			await context.SaveChangesAsync();
+
+			return NoContent();
+		}
 	}
 }
